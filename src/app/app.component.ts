87f22ee6a24e696ff3f07dc2188c4126f9c29d29
Roman Vavrynkevych <roman.vavrynkevych@gmail.com>
2022-09-11 18:18:55 +0300

    implemented route page save to local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NavigationStart, Router} from "@angular/router";
+import {StorageService} from "./services/storage.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title: string = 'Questionnaire Test Task';
+  routerSub!: Subscription;
+
+  constructor(private router: Router, private storageService: StorageService) {
+  }
+
+  ngOnInit() {
+    this.routerSub = this.router.events.subscribe(event => {
+      if(event instanceof NavigationStart) {
+        this.storageService.saveCurrentRoute(event.url);
+      }
+    })
+
+    this.storageService.restoreCurrentRoute();
+  }
+
+  ngOnDestroy() {
+    this.routerSub.unsubscribe();
+  }
 }
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  saveCurrentRoute(url: string): void {
+    localStorage.setItem('CurrentRoute', url)
+  }
+
+  restoreCurrentRoute(): void {
+    const url: string | null = localStorage.getItem('CurrentRoute');
+    this.router.navigate([url]).then(r => {
+      console.log('CurrentRoute restored ', r)
+    })
+  }
 }
