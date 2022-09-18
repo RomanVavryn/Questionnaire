d7c5ba012ce3a6145cdc069eade6538fd65ece8b
Roman Vavrynkevych <roman.vavrynkevych@gmail.com>
2022-09-18 19:47:56 +0300

    implemented v1 question-edit-page.component (basically by copy\paste)

--- src/app/pages/question-edit-page/question-edit-page.component.ts
+++ src/app/pages/question-edit-page/question-edit-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {StorageService} from "../../services/storage.service";
 import {QuestionInterface} from "../../types/question.interface";
+import {QuestionTypesEnum} from "../../types/question-types.enum";
+import {MatRadioChange} from "@angular/material/radio";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-question-edit-page',
@@ -9,13 +13,15 @@ import {QuestionInterface} from "../../types/question.interface";
 })
 export class QuestionEditPageComponent implements OnInit {
   title: string = 'Question edit';
-
+  questionTypes: typeof QuestionTypesEnum = QuestionTypesEnum;
   questions: QuestionInterface[] | null = null;
   editItem: QuestionInterface | null = null;
   itemIndex!: number | null;
 
   constructor(
     private storageService: StorageService,
+    private _snackBar: MatSnackBar,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -31,6 +37,36 @@ export class QuestionEditPageComponent implements OnInit {
       questions ? this.itemIndex = questions.indexOf(itemToEdit) : this.itemIndex = null;
     } else {
       questions ? this.questions = questions : this.questions = null;
+    }
+  }
+
+  onChangeHandler(event: MatRadioChange) {
+    if (this.editItem) {
+      this.editItem.type = event.value;
+    }
+  }
+
+  addNewOption(option: string) {
+    if (this.editItem && this.editItem.type === QuestionTypesEnum.Open) {
+      this.editItem.answerOptions.push({option})
+    } else if (this.editItem) {
+      this.editItem.answerOptions.push({option , checked: false})
+    }
+  }
+
+  deleteOption(option: string) {
+    if (this.editItem) {
+      this.editItem.answerOptions = this.editItem.answerOptions.filter(value => value.option !== option);
+    }
+  }
+
+  saveChanges(): void {
+    if (this.editItem) {
+      this.storageService.saveAnsweredQuestion(this.editItem);
+      this.router.navigate(['/management']).then(r => {
+        console.log('redirected ', r);
+        this._snackBar.open('Question was edited!', 'message', {duration: 3500});
+      });
     }
   }
 }
